2ee469e815dfc6df078f9bcd1b3f4fc5b3bc1924
Mostak Mahmud Raihan <m69450121@gmail.com>
2026-07-11 22:37:45 +0600

    Add BinarySearch implementation with main method for element search

---
+++ BinarySearch.java
@@ -0,0 +1,36 @@
+public class BinarySearch {
+    public static void main(String[] args){
+
+        int nums[] = {5,6,7,8,10,12};
+        int target = 12;
+        int low = 0;
+        int high = nums.length - 1;
+
+        int result = binarySearch(nums, target);
+
+        if(result != -1){
+            System.out.println("Element found at the index: " + result);
+        }else{
+            System.out.println("Element not found!");
+        }
+    }
+
+    public static int binarySearch(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
