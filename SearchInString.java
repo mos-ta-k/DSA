e778bae44699f6a09be402f46196295f620e41f4
Mostak Mahmud Raihan <m69450121@gmail.com>
2026-07-19 23:07:06 +0600

    search in string

---
+++ SearchInString.java
@@ -0,0 +1,27 @@
+import java.sql.Blob;
+
+public class SearchInString {
+    public static void main(String[] args){
+
+        String sub = "DataStructure";
+        char target = 'm';
+
+        System.out.println(search(sub, target));
+    }
+
+    static boolean search(String str, char target){
+
+        if(str.length() == 0){
+            return false;
+        }
+
+        for(int i = 0; i<str.length(); i++){
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
