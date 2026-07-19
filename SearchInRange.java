196ed1b8ffdf972dad280078f12587beba07f482
Mostak Mahmud Raihan <m69450121@gmail.com>
2026-07-19 23:20:08 +0600

    search in a range inside of an array

---
+++ SearchInRange.java
@@ -0,0 +1,21 @@
+
+public class SearchInRange {
+    public static void main(String[] args){
+        int[] arr = {18, 12, -7, 3, 14,28};
+        int target = 3;
+        System.out.println(searchInRange(arr, target, 1, 5));
+    }
+    static int searchInRange(int[] arr, int target, int start, int end){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int index = start; index<=end; index++){
+            int element = arr[index];
+            if(element == target){
+                return index;
+            }
+        }
+        return -1;
+    }
+}
